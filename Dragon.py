0d78806eef312b7c0dc97775201be8a8b0f99d92
king-cobra-user <78993442+king-cobra-user@users.noreply.github.com>
2021-03-13 18:57:43 +0530

    Create Dragon.py

---
+++ Dragon.py
@@ -0,0 +1,48 @@
+# COPYRIGHT (C) 2021 BY @real_tera_baap
+
+"""
+
+(((((((((((((((((((((((@real_tera_baap)))))))))))))))))))))))))))
+
+(((((((((((((((((((((((@real_tera_baap)))))))))))))))))))))))))))
+
+(((((((((((((((((((((((@real_tera_baap)))))))))))))))))))))))))))
+
+(((((((((((((((((((((((@real_tera_baap)))))))))))))))))))))))))))
+
+"""
+
+# MADE BY @real_tera_baap ðŸ”¥
+
+# MY IDEA H YRR DONT KANG THIS PLEASE
+
+import asyncio
+
+from userbot.utils import admin_cmd as DRAGONX
+
+from userbot import bot as DRAGONBOT
+
+from telethon import events, custom, Button
+
+import time
+
+import os
+
+from userbot import botnickname 
+
+BOT = str(botnickname) if botnickname else "DRAGON BOT"
+
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DRAGON BOY"
+
+PHOTTO = os.environ.get("ALIVE_PHOTTO", "https://telegra.ph/file/ab0a3baf1ff660d64a237.jpg")
+
+LEGENDX = "[@real_tera_baap ](@real_tera_baap)"
+
+REPO = "[DRAGON BOT](https://github.com/king-cobra-user/DRAGON-USER-BOT)"
+
+PRO = bot.uid
+
+MASTER = f"[{NAME}](tg://user?id={PRO})"
+
+GROUP = "[SUPPORT GROUP]( @DRAGON_USERBOT_UPDATES  )
+
