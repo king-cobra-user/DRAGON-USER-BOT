9b0bc3e6fc5bb4b86a4e595b05a0e1045ae182e2
king-cobra-user <78993442+king-cobra-user@users.noreply.github.com>
2021-03-13 19:35:39 +0530

    Create _Dragon.py

---
+++ _Dragon.py
@@ -0,0 +1,92 @@
+#chala jaa bhosdike 
+
+#import krte h pehle 馃憞
+
+import asyncio
+
+from telethon import events
+
+from telethon.tl.types import ChannelParticipantsAdmins
+
+from userbot import ALIVE_NAME, StartTime
+
+from userbot.utils import admin_cmd
+
+from userbot import bot
+
+from telethon import version
+
+from math import ceil
+
+import json
+
+import random
+
+import re
+
+from telethon import events, errors, custom
+
+import io
+
+from userbot.helpers.functions import get_readable_time
+
+import time
+
+import os
+
+import datetime
+
+#importing finished
+
+from userbot import botnickname 
+
+BOT = str(botnickname) if botnickname else "DRAGON BOT"
+
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DRAGON BOY"
+
+tim = get_readable_time((time.time() - StartTime))
+
+#pic 馃憞
+
+PIC = os.environ.get("ALIVE_PIC")
+
+#op 
+
+uptime = tim
+
+#time = date + time okay
+
+TIME = time.asctime(time.localtime())
+
+#my name 馃憞
+
+DRAGON = "[DRAGON X](@real_tera_baap)"
+
+#my bots repo 馃憞
+
+REPO = "[LEGENDhttps://github.com/king-cobra-user/DRAGON-USER BOT](https://github.com/king-cobra-user/DRAGON-USER-BOT)"
+
+#grpup馃憞NAME = "[{MAATER}](tg://user?id={X})"
+
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+
+#aur aage add kruga abhi busy okay 馃
+
+global ghanti
+
+X = bot.uid
+
+MASTER = f"[{NAME}](tg://user?id={X})"
+
+GROUP = "[SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)"
+
+#itna test h aur aage krte h
+
+#test successful raha ab aage 
+
+ALIVE = "LEGEND BOT IS ON 馃敟 FIRE 馃敟" #make by 𝕂ÍÑ𝔾🔥🔥ÇÓ𝔹ℝÆ 𝔾𝔸ℕ𝔾
+
+OP = " HELLO MASTER MY NAME IS LEGEND BOT I AM A BEST USERBOT 馃挐"
+
+EMOJI = "馃敟"
+
